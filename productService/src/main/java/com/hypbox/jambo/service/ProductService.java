5a796dec29cfa97056823d7daf953d982bf4d987
Amir Hosseinzadeh <amir.hosseinzadeh@c2ro.com>
2018-11-26 12:03:35 -0500

    add new products

--- productService/src/main/java/com/hypbox/jambo/service/ProductService.java
+++ productService/src/main/java/com/hypbox/jambo/service/ProductService.java
@@ -72,6 +72,22 @@ public class ProductService {
         p1.setImageUrl("https://multimedia.bbycastatic.ca/multimedia/products/1500x1500/128/12872/12872862.jpg");
         p1.setSupplierUrl("https://www.bestbuy.ca/en-ca/product/seiko-43mm-men-s-chronograph-casual-watch-hard-coated-silver-black/12872862.aspx?");
         this.add(p1);
+
+        p1 = new Product();
+        p1.setTitle("Spring Festival Traditional Clothes-Dragon robe");
+        p1.setDescription("The Qing Dynasty Costume/Queen's dress/Ancient Chinese Cosplay/Emperors Clothing/New Year's dress/Halloween Costume/Campus Party Dress");
+        p1.setPrice(97.50);
+        p1.setImageUrl("https://images-na.ssl-images-amazon.com/images/I/612gVDCvtML._SX679_.jpg");
+        p1.setSupplierUrl("https://www.amazon.com/Emperors-Clothing-Halloween-Traditional-Clothes-Dragon/dp/B07C5Z1BVK/ref=sr_1_20?ie=UTF8&qid=1543246731&sr=8-20&keywords=china+new+year");
+        this.add(p1);
+
+        p1 = new Product();
+        p1.setTitle("Capelin Caviar 1.75 oz each");
+        p1.setDescription("Get it as soon as Dec 11 - 21 when you choose Standard Shipping at checkout. Ships from and sold by Super Sales 365");
+        p1.setPrice(56.83);
+        p1.setImageUrl("https://www.caviarrusse.com/spree/products/1/product/Caspian_Sea_000_Beluga_Caviar.jpg?1480732399");
+        p1.setSupplierUrl("https://www.amazon.ca/Capelin-Caviar-1-75-Items-Order/dp/B01ASW5MFW/ref=sr_1_11?s=grocery&ie=UTF8&qid=1543247252&sr=1-11&keywords=caviar");
+        this.add(p1);
     }
 
 }
